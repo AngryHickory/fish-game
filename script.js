ecddaceaff414a343b3972b0d73e37141a3b51d4
AngryHickory <138169281+AngryHickory@users.noreply.github.com>
2025-05-23 16:22:30 -0700

    Initialize needed variables and link buttons
    Declared some variables like player stats and fishing stats.
    Linked the buttons using .querySelector so that they will be functional.

---
+++ script.js
@@ -0,0 +1,11 @@
+let xp = 0;
+let bait = 100;
+let gold = 50;
+let currentRodIndex = 0;
+let fishing;
+let fishHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("button1");
+const button2 = document.querySelector("button2");
+const button3 = document.querySelector("button3");
