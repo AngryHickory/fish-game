82ff62ebec2369abf65403de33f2fbb6d58101fb
AngryHickory <138169281+AngryHickory@users.noreply.github.com>
2025-05-24 19:33:40 -0700

    Add goTown function
    This is the function that acts as the town square,
    it's a hub you can go to the other locations from.

--- script.js
+++ script.js
@@ -11,18 +11,35 @@ const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
+const baitText = document.querySelector("#baitText");
 const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
+const fishStats = document.querySelector("#fishStats");
+const fishName = document.querySelector("#fishName");
+const fishHealthText = document.querySelector("#fishHealth");
+
+
+// initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goFish;
+button3.onclick = openSeas;
+
+function goTown() {
+  button1.innerText = "Go to Store";
+  button2.innerText = "Go Fishing";
+  button3.innerText = "Open Seas";
+  button1.onclick = goStore;
+  button2.onclick = goFish;
+  button3.onclick = openSeas;
+  text.innerText = "You are in the town square. You see a sign that says Store."
+}
 
 function goStore() {
   button1.innerText = "Buy 10 Bait (10 Gold)";
   button2.innerText = "Buy New Rod (30 Gold)";
   button3.innerText = "Go to Town Square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
+  button1.onclick = buyBait;
+  button2.onclick = buyRod;
   button3.onclick = goTown;
   text.innerText = "You enter the store."
 
@@ -37,10 +54,10 @@ function openSeas() {
   console.log("Sailing the sea.");
 }
 
+function buyBait() {
 
-// initialize buttons
+}
 
-button1.onclick = goStore;
-button2.onclick = goFish;
-button3.onclick = openSeas;
+function buyRod() {
 
+}
